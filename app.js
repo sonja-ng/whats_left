f2965b1d7098595d8b0b1208d631249b17eafe77
Sonja <sonja.ng1@gmail.com>
2022-02-11 18:12:08 -0500

    Add user authentication

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const passport = require('passport');
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
-app.get("/", (req, res) => res.send("Hello World!!"));
+
+
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
